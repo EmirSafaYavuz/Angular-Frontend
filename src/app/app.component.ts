a5b70d2707e2406e4848cb9422dd5d90be1fd34c
EmirSafaYavuz <emirsafayavuz@outlook.com>
2021-03-07 01:06:40 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+//component html in datasını yönettiğin yer
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = "Emir Safa Yavuz";
+  
+  
 }
+
